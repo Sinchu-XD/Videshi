a82b26cd91d7c961d08b176b5d7dc01519ed53ce
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-07-14 22:32:57 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,14 +1,28 @@
-from Bot import bot
 import os
 import importlib
+import asyncio
+from Bot import bot
 
 plugin_folder = "Plugins"
 
-for filename in os.listdir(plugin_folder):
-    if filename.endswith(".py"):
-        importlib.import_module(f"{plugin_folder}.{filename[:-3]}")
+# Asynchronous function to load plugins
+async def load_plugins():
+    for filename in os.listdir(plugin_folder):
+        if filename.endswith(".py"):
+            importlib.import_module(f"{plugin_folder}.{filename[:-3]}")
 
-if __name__ == "__main__":
+# Initialize function to load plugins and run the bot
+async def init():
     print(">> Bot Starting...")
-    bot.run()
-  
+
+    # Load plugins asynchronously
+    await load_plugins()
+
+    # Run the bot
+    await bot.run()
+
+# Main entry point
+if __name__ == "__main__":
+    # Create the event loop and run the init function
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(init())  # Run the async init function until complete
