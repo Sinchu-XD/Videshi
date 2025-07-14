75112ff15e3b074236ad0f0b362f7d25f58e8dc0
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-07-15 00:41:07 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -3,9 +3,10 @@ import os
 class Config:
     API_ID = int(os.getenv("API_ID", 6067591))
     API_HASH = os.getenv("API_HASH", "94e17044c2393f43fda31d3afe77b26b")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "7946373751:AAEnaR6L6-8d9cYYlOCS1UqmXSjRXFjARVI")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "8086063285:AAGHCBnMD28tVHu3mnfxyA7kRsB2hZW2DWA")
     MONGO_URI = os.getenv("MONGO_URL", "mongodb+srv://Sinchu:Sinchu@sinchu.qwijj.mongodb.net/?retryWrites=true&w=majority&appName=Sinchu")
-    OWNER_ID = int(os.getenv("OWNER_ID", "7862043458"))
+
+    OWNER_ID = int(os.environ.get("OWNER_ID", "7862043458"))  # Your Telegram user ID
     LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID", -1002716981360))
-    BOT_USERNAME = "InstagramStuffsBot"
-  
+    
+    BOT_USERNAME = "VideshiStuffBot"
