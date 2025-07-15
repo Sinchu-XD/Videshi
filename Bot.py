b0e217fd55f2de69f5a568cf58c7b5e685d3b357
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-07-15 12:24:03 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -7,3 +7,14 @@ bot = Client(
     api_hash=Config.API_HASH,
     bot_token=Config.BOT_TOKEN
 )
+
+from telethon import TelegramClient
+
+app = TelegramClient(
+    "TelethonSession",
+    Config.API_ID,
+    Config.API_HASH
+)
+
+# This will keep the Telethon session active
+app.start(bot_token=Config.BOT_TOKEN)
