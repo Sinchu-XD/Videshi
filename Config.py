08777df742c3ac63907d1e92a82dee0df35da572
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-07-26 11:37:27 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -8,7 +8,7 @@ class Config:
     BOT_TOKEN = os.getenv("BOT_TOKEN", "7443259882:AAE8tgZbbhKVaYbWdfwCe6rwJt7ADSRicYM")
     MONGO_URI = os.getenv("MONGO_URL", "mongodb+srv://Sinchu:Sinchu@sinchu.qwijj.mongodb.net/?retryWrites=true&w=majority&appName=Sinchu")
 
-    OWNER_ID = int(os.environ.get("OWNER_ID", "7862043458"))  # Your Telegram user ID
+    OWNER_ID = int(os.environ.get("OWNER_ID", "7643047806"))  # Your Telegram user ID
     LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID", -1002716981360))
     
     BOT_USERNAME = "StreeStuffBot"
